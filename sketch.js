8fea369eb1f3a01e5341c95281adbb9c74977bf9
Ignacio Streuly <ijstreuly@gmail.com>
2016-12-12 20:22:55 -0500

    Add: disclosure filter

--- sketch.js
+++ sketch.js
@@ -15,6 +15,18 @@ var rainbowImage, rainbowSound;
 var moustacheImage;
 var rainbows = [];
 
+var facePoints = {
+	'faceOutline'  : {'closed':false, points: [0,1,2,3,4,5,6,7,8,9,10,11,12,13]},  
+	'leftEye'      : {'closed':true,  points: [23,63,24,64,25,65,26,66]},
+	'rightEye'     : {'closed':true,  points: [30,68,29,67,28,70,31,69]},
+	'leftEyebrow'  : {'closed':false, points: [19,20,21,22]},
+	'rightEyebrow' : {'closed':false, points: [18,17,16,15]},
+	'noseBridge'   : {'closed':false, points: [33,41,62]},
+	'nose'         : {'closed':false, points: [34,35,36,42,37,43,38,39,40]},
+	'upperLip'     : {'closed':true,  points: [44,45,46,47,48,49,50,59,60,61]},
+	'lowerLip'     : {'closed':true,  points: [44,55,54,53,52,51,50,58,57,56]}	
+};
+
 
 function preload() {
 
@@ -136,6 +148,37 @@ function draw() {
 	    	var upperLipNoseDistance = dist(faceArray[37][0], faceArray[37][1], faceArray[47][0], faceArray[47][1]);
 	    	var mouthWidth = dist(faceArray[44][0], faceArray[44][1], faceArray[50][0], faceArray[50][1]);
 			image(moustacheImage, faceArray[37][0], faceArray[37][1], mouthWidth, upperLipNoseDistance);		
+		}
+
+		if (state ==4){
+			stroke(255,255,255);
+
+			// each element of the faceArray contains two sub-elements - the x
+			// position and the y position
+			
+			// draw all points that have been defined above
+			for (var key in facePoints)
+			{
+				// draw the shape
+				var arrayLength = facePoints[key]['points'].length;
+				for (var i = 0; i < arrayLength-1; i++)
+				{	
+					line(faceArray[ facePoints[key]['points'][i] ][0], faceArray[ facePoints[key]['points'][i] ][1], faceArray[ facePoints[key]['points'][i+1] ][0], faceArray[ facePoints[key]['points'][i+1] ][1]);					
+				}
+				
+				// if the shape is closed we should connect the last point to the first point
+				if (facePoints[key]['closed'])
+				{
+					line(faceArray[ facePoints[key]['points'][ arrayLength-1 ] ][0], faceArray[ facePoints[key]['points'][ arrayLength-1 ] ][1], faceArray[ facePoints[key]['points'][0] ][0], faceArray[ facePoints[key]['points'][0] ][1]);
+				}
+			}
+			
+			// draw pupils
+			fill(255);
+			noStroke();
+			
+			ellipse(faceArray[27][0], faceArray[27][1], 5, 5);
+			ellipse(faceArray[32][0], faceArray[32][1], 5, 5);
 		}
 	}
 
